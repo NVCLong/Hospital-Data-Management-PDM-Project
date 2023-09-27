0047524fd9db8fbc149933fff39e8898bf5cd205
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2023-09-27 08:54:23 +0700

    Create project structure

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 6000;
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
