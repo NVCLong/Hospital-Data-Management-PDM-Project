af94df934224d1d124c23322fa8ab6c11f0c0e20
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2023-11-04 00:15:29 +0700

    Update authentication by using mysql

--- server/app/Controller/MiddlewareController.js
+++ server/app/Controller/MiddlewareController.js
@@ -1,4 +1,5 @@
-
+const userController= require('../Controller/UserController')
+const jwt= require('jsonwebtoken')
 
 
 // verify token as a middleware
@@ -6,9 +7,31 @@
 // wait until with
 
 class MiddlewareController{
-    verifyAccessToken(req,res){
+    verifyRefreshToken(req,res){
+        const token = req.cookies.refreshToken;
+        if(token){
+            jwt.verify(token,'secret',(err)=>{
+                if(err){
+                    res.redirect('authentication')
+                }
+                userController.requestRefreshToken(req,res).then(
+                    function () {
+                        console.log("Successfully")
+                    }
+                ).catch(function (err) {
+                    console.log(err)
+                })
+            })
+        }else{
+            res.redirect('/authentication')
+        }
+    }
 
+
+    // get token in redux session
+    verifyAccessToken(req, res){
 
     }
+
 
 }
